29572cbb0e4cdd1d0aadd68db8a34a7e095d3a4a
bhavesh-birla <birlabhavesh280@gmail.com>
2025-11-20 17:06:20 +0530

    changes

--- backend/bl-birla/src/main/java/com/blbirla/app/config/CorsConfig.java
+++ backend/bl-birla/src/main/java/com/blbirla/app/config/CorsConfig.java
@@ -2,7 +2,8 @@ package com.blbirla.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -14,9 +15,9 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
             }
         };
     }
 }
-
